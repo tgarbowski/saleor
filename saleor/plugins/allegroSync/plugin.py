7f3af326c77b47d5de9099abfdbfdc5efc846124
Michał Dąbrowski <michal0211@gmail.com>
2021-05-21 14:31:28 +0200

    fix synchronize allegro offers, set offers type, db query reduce

--- saleor/plugins/allegroSync/plugin.py
+++ saleor/plugins/allegroSync/plugin.py
@@ -2,7 +2,8 @@ import json
 import logging
 from dataclasses import dataclass
 from email.mime.multipart import MIMEMultipart
-from saleor.product.models import ProductVariant
+
+from saleor.product.models import ProductVariant, Product
 from saleor.plugins.manager import PluginsManager
 from saleor.plugins.allegro.plugin import AllegroPlugin, AllegroAPI
 from saleor.plugins.base_plugin import BasePlugin
@@ -63,45 +64,47 @@ class AllegroSyncPlugin(BasePlugin):
         plugin_configs = manage.get_plugin(AllegroPlugin.PLUGIN_ID)
         conf = {item["name"]: item["value"] for item in plugin_configs.configuration}
         token = conf.get('token_value')
-        allegro_api = AllegroAPI(token)
-        params = {'publication.status': [str(self.type)], 'limit': '1',
-                  'offset': 0}
+        env = conf.get('env')
+        allegro_api = AllegroAPI(token, env)
+        params = {'publication.status': ['ACTIVE'], 'limit': '1', 'offset': 0}
         response = allegro_api.get_request('sale/offers', params)
-        totalCount = json.loads(response.text).get('totalCount')
+        total_count = json.loads(response.text).get('totalCount')
 
-        if totalCount == None:
+        if total_count is None:
             return
         limit = 1000
         errors = []
 
-        for i in range(int(int(totalCount) / limit) + 1):
+        for i in range(int(int(total_count) / limit) + 1):
             offset = i * limit
-
-            params = {'publication.status': [str(self.type)], 'limit': limit,
-                      'offset': offset}
+            params = {'publication.status': ['ACTIVE'], 'limit': limit, 'offset': offset}
             response = allegro_api.get_request('sale/offers', params)
             offers = json.loads(response.text).get('offers')
             if offers:
                 skus = [offer.get('external').get('id') for offer in offers]
-                product_variants = list(ProductVariant.objects.filter(sku__in=skus))
+                product_variants = list(ProductVariant.objects.select_related('product').filter(sku__in=skus))
+                products_to_update = []
                 for offer in offers:
                     product_errors = []
                     sku = offer.get('external').get('id')
-                    id = offer.get('id')
+                    offer_id = offer.get('id')
                     variant = next((x for x in product_variants if x.sku == sku), None)
                     if variant:
                         product = variant.product
                         product_errors = AllegroSyncPlugin.valid_product(product)
-                        if len(product_errors) == 0:
-                            if product.private_metadata.get('publish.allegro.id') != id:
-                                product.store_value_in_private_metadata({
-                                    'publish.allegro.id': id})
-                                product.save(update_fields=["private_metadata"])
+                        if not product_errors:
+                            if product.private_metadata.get('publish.allegro.id') != offer_id:
+                                product.private_metadata['publish.allegro.id'] = offer_id
+                                products_to_update.append(product)
                     else:
                         product_errors.append('nie znaleziono produktu o podanym SKU')
+                        errors.append({'sku': sku, 'errors': product_errors})
 
-                    errors.append({'sku': sku, 'errors': product_errors})
+                if products_to_update:
+                    Product.objects.bulk_update(products_to_update, ['private_metadata'])
+
         html_errors_list = plugin_configs.create_table(errors)
+
         return self.send_mail(html_errors_list)
 
     def send_mail(self, html_errors_list):
@@ -119,3 +122,4 @@ class AllegroSyncPlugin(BasePlugin):
         msg['To'] = 'sync+noreply.salingo@gmail.com'
 
         server.sendmail('noreply.salingo@gmail.com', 'noreply.salingo@gmail.com', msg.as_string())
+
