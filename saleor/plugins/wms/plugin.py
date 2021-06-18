e5688387225b93dde516e083bc3be04682594821
Michał Dąbrowski <michal0211@gmail.com>
2021-06-18 11:12:54 +0200

    WMSDocument plugin base config

--- saleor/plugins/wms/plugin.py
+++ saleor/plugins/wms/plugin.py
@@ -1,4 +1,15 @@
-from ..base_plugin import BasePlugin
+from dataclasses import dataclass
+
+from ..base_plugin import BasePlugin, ConfigurationTypeField
+
+
+@dataclass
+class WMSConfiguration:
+    GRN: str
+    GIN: str
+    IWM: str
+    FGTN: str
+    IO: str
 
 
 class WMSPlugin(BasePlugin):
@@ -6,11 +17,38 @@ class WMSPlugin(BasePlugin):
     PLUGIN_ID = "WMS"
     DEFAULT_ACTIVE = True
     DEFAULT_CONFIGURATION = [
-        {"name": "Goods received note (GRN)", "value": None, "label": "Goods received note (GRN)"}
+        {"name": "GRN", "value": "GRN"},
+        {"name": "GIN", "value": "GIN"},
+        {"name": "IWM", "value": "IWM"},
+        {"name": "FGTN", "value": "FGTN"},
+        {"name": "IO", "value": "IO"}
     ]
     PLUGIN_DESCRIPTION = (
         "Warehouse management system plugin"
     )
+    CONFIG_STRUCTURE = {
+        "GRN": {
+            "type": ConfigurationTypeField.STRING,
+            "label": "Goods received note (GRN) eg. PZ",
+        },
+        "GIN": {
+            "type": ConfigurationTypeField.STRING,
+            "label": "Goods issued note (GIN) eg: WZ",
+        },
+        "IWM": {
+            "type": ConfigurationTypeField.STRING,
+            "label": "Internal warehouse movement (IWM) eg: MM",
+        },
+        "FGTN": {
+            "type": ConfigurationTypeField.STRING,
+            "label": "Finished goods transfer note (FGTN) eg: PW",
+        },
+        "IO": {
+            "type": ConfigurationTypeField.STRING,
+            "label": "Internal outgoings (IO) eg: RW",
+        }
+    }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        configuration = {item["name"]: item["value"] for item in self.configuration}
