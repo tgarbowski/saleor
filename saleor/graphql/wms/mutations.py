d443c5a04c189f46a37a771b2eed4fa9efbe7b9e
Michał Dąbrowski <michal0211@gmail.com>
2021-06-18 11:12:04 +0200

    Generate WMSDocument number and disallow adding positions do approved documents

--- saleor/graphql/wms/mutations.py
+++ saleor/graphql/wms/mutations.py
@@ -1,11 +1,13 @@
 import graphene
 
+from django.core.exceptions import ValidationError
 from saleor.graphql.core.mutations import ModelDeleteMutation, ModelMutation
 from .types import WMSDocumentInput, WMSDocPositionInput
 from saleor.wms import models
 from saleor.core.permissions import ProductPermissions
 from saleor.graphql.core.types.common import WMSDocumentError
 from saleor.core.exceptions import PermissionDenied
+from saleor.plugins.manager import get_plugins_manager
 
 
 class WMSDocumentCreate(ModelMutation):
@@ -21,8 +23,35 @@ class WMSDocumentCreate(ModelMutation):
         error_type_class = WMSDocumentError
         error_type_field = "product_errors"
 
+    @classmethod
+    def save(cls, info, instance, cleaned_input):
+        document_number = WMSDocumentCreate.create_document_number(instance.document_type)
+        instance.number = document_number
+        instance.save()
 
-class WMSDocumentUpdate(WMSDocumentCreate):
+    @staticmethod
+    def create_document_number(document_type):
+        configuration = WMSDocumentCreate.get_plugin_config('WMS')
+        user_document_type = configuration.get(document_type)
+
+        last_document = models.WMSDocument.objects.filter(document_type=document_type).order_by('id').last()
+        if not last_document:
+            return f'{user_document_type}1'
+        last_document_number = last_document.number
+        last_document_int = int(''.join(i for i in last_document_number if i.isdigit()))
+        new_document_int = last_document_int + 1
+        new_document_number = f'{user_document_type}{str(new_document_int)}'
+        return new_document_number
+
+    @staticmethod
+    def get_plugin_config(plugin):
+        manager = get_plugins_manager()
+        plugin = manager.get_plugin(plugin)
+        configuration = {item["name"]: item["value"] for item in plugin.configuration}
+        return configuration
+
+
+class WMSDocumentUpdate(ModelMutation):
     class Arguments:
         id = graphene.ID(required=True, description="ID of a WMSDocument to update.")
         input = WMSDocumentInput(
@@ -78,6 +107,19 @@ class WMSDocPositionCreate(ModelMutation):
         permissions = (ProductPermissions.MANAGE_PRODUCTS,)
         error_type_class = WMSDocumentError
         error_type_field = "product_errors"
+
+    @classmethod
+    def save(cls, info, instance, cleaned_input):
+        if instance.document.status == 'APPROVED':
+            raise ValidationError(
+                {
+                    "docposition": ValidationError(
+                        "You can't add or update position to accepted document"
+                    )
+                }
+            )
+
+        instance.save()
 
 
 class WMSDocPositionUpdate(WMSDocPositionCreate):
