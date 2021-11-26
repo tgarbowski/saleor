84cd23ab3a916ecb8f02cc8a2f1b1e77ad97b3bb
Michał Dąbrowski <michal0211@gmail.com>
2021-11-26 14:52:26 +0100

    Retrieve images from backup command

--- saleor/core/management/commands/retrieve_images_from_backup.py
+++ saleor/core/management/commands/retrieve_images_from_backup.py
@@ -1,60 +1,94 @@
-import boto3
+from collections import namedtuple
+from io import BytesIO
 
-from django.core.management.base import BaseCommand
+import boto3
+from PIL import Image
+
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
 
 from saleor.product.models import ProductMedia
-from saleor.core.utils import create_thumbnails
-from collections import namedtuple
 
 
 class Command(BaseCommand):
     version = "1.0"
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.s3 = boto3.client('s3')
+        self.bucket = settings.AWS_MEDIA_BUCKET_NAME
+        self.backup_bucket = settings.AWS_BACKUP_BUCKET_NAME
+
     def add_arguments(self, parser):
         parser.add_argument(
             "--image",
             type=str,
-            help=('Image path')
-        )
-        parser.add_argument(
-            "--bucket",
-            type=str,
-            help=('Bucket name')
+            help=('Image path'),
+            required=True
         )
 
     def handle(self, *args, **options):
         image = options.get("image")
-        bucket = options.get("bucket")
-        media = ProductMedia.objects.get(image=image)
-        file = self.get_file_from_s3(key=image, bucket=bucket)
-        self.upload_from_memory_to_s3(key=image, bucket=bucket, data=file)
-        self.update_thumbnails(image)
-        create_thumbnails(pk=media.id, model=ProductMedia, size_set="products")
+        self.validate_image(image)
+        file = self.get_file_from_s3(key=image, bucket=self.backup_bucket)
+        self.upload_from_memory_to_s3(key=image, bucket=self.bucket, data=file)
+        self.update_thumbnails(image=file, image_name=image)
+
+    @staticmethod
+    def validate_image(image):
+        if not image:
+            raise CommandError("Please provide image path.")
+
+        try:
+            media = ProductMedia.objects.get(image=image)
+        except ObjectDoesNotExist:
+            raise CommandError("Provided image doesnt match any product.")
 
     def get_file_from_s3(self, key, bucket):
-        s3 = boto3.client('s3')
-        image = s3.get_object(
-            Bucket=bucket,
-            Key=key
-        )
+        image = self.s3.get_object(Bucket=bucket, Key=key)
         data = image['Body'].read()
 
         return data
 
     def upload_from_memory_to_s3(self, key, bucket, data):
-        s3 = boto3.client('s3')
-        s3.put_object(
+        self.s3.put_object(
             Bucket=bucket,
             Key=key,
             Body=data
         )
 
-    def update_thumbnails(self, image):
-        client = boto3.client('s3')
+    @staticmethod
+    def create_thumbnail(buffered_image, size):
+        thumbnail = BytesIO()
+        main_image = Image.open(buffered_image)
+        main_image.thumbnail(size)
+        main_image.save(thumbnail, format=main_image.format)
+        thumbnail.seek(0)
+
+        return thumbnail
+
+    @staticmethod
+    def get_thumbnail_sizes():
         Size = namedtuple('Size', ['x', 'y'])
-        sizes = [Size(*t) for t in
-                 [(60, 60), (120, 120), (255, 255), (510, 510), (540, 540),
-                  (1080, 1080)]]
+        thumbnail_sizes = [(60, 60), (120, 120), (255, 255), (510, 510), (540, 540), (1080, 1080)]
+        sizes = [Size(*t) for t in thumbnail_sizes]
+
+        return sizes
+
+    def update_thumbnails(self, image, image_name):
+        sizes = self.get_thumbnail_sizes()
 
         for size in sizes:
-            key = f'__sized__/{image}-thumbnail-{size.x}x{size.y}-70.jpg'
+            buffered_image = BytesIO(image)
+            thumbnail = self.create_thumbnail(buffered_image, size)
+            name = self.create_thumbnail_name(image_name)
+            key = f'__sized__/{name}-thumbnail-{size.x}x{size.y}-70.jpg'
+            self.upload_from_memory_to_s3(key=key, bucket=self.bucket, data=thumbnail)
+
+    @staticmethod
+    def create_thumbnail_name(image):
+        if '.' in image:
+            return image.split('.', 1)[0]
+        else:
+            return image
