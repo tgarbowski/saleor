cd0d9f0ec092b555476414406c65e2c0a1e7f777
Michał Dąbrowski <michal0211@gmail.com>
2021-12-16 17:35:44 +0100

    salingo pricing plugin

--- saleor/plugins/salingo_pricing/plugin.py
+++ saleor/plugins/salingo_pricing/plugin.py
@@ -1,4 +1,10 @@
-from saleor.salingo.business_rules import BusinessRulesBasePlugin
+from decimal import Decimal, InvalidOperation
+
+from django.core.exceptions import ValidationError
+
+from saleor.salingo.business_rules import (BusinessRulesBasePlugin, BusinessRulesEvaluator,
+                                           PriceEnum)
+from saleor.plugins.error_codes import PluginErrorCode
 
 
 class SalingoPricingPlugin(BusinessRulesBasePlugin):
@@ -6,7 +12,64 @@ class SalingoPricingPlugin(BusinessRulesBasePlugin):
     PLUGIN_ID = "salingo_pricing"
     DEFAULT_ACTIVE = False
     PLUGIN_DESCRIPTION = ("Salingo pricing configuration")
-    # CONFIGURATION_PER_CHANNEL = True
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+    @classmethod
+    def validate_plugin_configuration(self, plugin_configuration: "PluginConfiguration"):
+        config = plugin_configuration.configuration
+        yaml_rules = BusinessRulesEvaluator.get_value_by_name(config=config, name='rules')
+        engine_rules = BusinessRulesEvaluator.get_rules(yaml_rules)
+        prices = [rule['result'] for rule in engine_rules]
+
+        for price in prices:
+            self.validate_price(price)
+
+        try:
+            BusinessRulesEvaluator.validate_rules(engine_rules)
+        except Exception as e:
+            raise ValidationError(
+                {
+                    "rules": ValidationError(
+                        "Invalid engine rule.",
+                        code=PluginErrorCode.INVALID.value,
+                    )
+                }
+            )
+
+    @classmethod
+    def validate_price(cls, price):
+        if not isinstance(price, str):
+            raise ValidationError(
+                {
+                    "rules": ValidationError(
+                        "Invalid pricing result.",
+                        code=PluginErrorCode.INVALID.value,
+                    )
+                }
+            )
+
+        try:
+            result_price = Decimal(price[1:])
+        except InvalidOperation:
+            raise ValidationError(
+                {
+                    "rules": ValidationError(
+                        "Invalid pricing result price.",
+                        code=PluginErrorCode.INVALID.value,
+                    )
+                }
+            )
+
+        price_mode = price[:1]
+
+        if price_mode not in [e.value for e in PriceEnum]:
+            raise ValidationError(
+                {
+                    "rules": ValidationError(
+                        "Invalid price mode.",
+                        code=PluginErrorCode.INVALID.value,
+                    )
+                }
+            )
--- saleor/salingo/tests.py
+++ saleor/salingo/tests.py
@@ -1,3 +1,53 @@
-from django.test import TestCase
+from decimal import Decimal
 
-# Create your tests here.
+from measurement.measures import Weight
+from pytest import approx
+
+from saleor.salingo.business_rules import Executors
+
+
+def test_calculate_price_weight_mode():
+    validated_pricing_variables = Executors.get_validated_pricing_variables(
+        current_price=Decimal(1.00),
+        weight=Weight(kg=2),
+        result='k12.40'
+    )
+
+    price = Executors.calculate_price(validated_pricing_variables)
+
+    assert price == approx(Decimal(24.80))
+
+
+def test_calculate_price_weight_null():
+    validated_pricing_variables = Executors.get_validated_pricing_variables(
+        current_price=Decimal(1.00),
+        weight=None,
+        result='k12.40'
+    )
+
+    assert validated_pricing_variables is None
+
+
+def test_calculate_price_discount_mode():
+    validated_pricing_variables = Executors.get_validated_pricing_variables(
+        current_price=Decimal(10.00),
+        weight=Weight(kg=2),
+        result='d10'
+    )
+
+    price = Executors.calculate_price(validated_pricing_variables)
+
+    assert price == approx(Decimal(9.00))
+
+
+def test_calculate_price_item_mode():
+    validated_pricing_variables = Executors.get_validated_pricing_variables(
+        current_price=Decimal(25.99),
+        weight=Weight(kg=2),
+        result='i25.99'
+    )
+
+    price = Executors.calculate_price(validated_pricing_variables)
+
+    assert price == approx(Decimal(25.99))
+
