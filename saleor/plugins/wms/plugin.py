875567cc2377561af32a1e9314e1fa1a3cfa4eec
Tooomas <38753830+T000mas@users.noreply.github.com>
2022-05-09 09:06:47 +0200

    Feature/5869 wz generation (#212)
    * Add on order fulfilled wz generation
    
    * Fix
    
    * extend wms model
    
    Co-authored-by: Michał Dąbrowski <michal0211@gmail.com>

--- saleor/plugins/wms/plugin.py
+++ saleor/plugins/wms/plugin.py
@@ -1,6 +1,13 @@
 from dataclasses import dataclass
+from datetime import datetime
+import re
+
+from django.db import transaction
 
 from ..base_plugin import BasePlugin, ConfigurationTypeField
+from ...order.models import Order, OrderLine
+from ...wms.models import WmsDocument, WmsDocPosition
+from saleor.warehouse.models import Warehouse
 
 
 @dataclass
@@ -52,3 +59,91 @@ class WMSPlugin(BasePlugin):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         configuration = {item["name"]: item["value"] for item in self.configuration}
+
+    def fulfillment_canceled(
+        self,
+        fulfillment,
+        previous_value,
+    ):
+        # check if there is already a wms document and delete if true
+        wms_document = WmsDocument.objects.filter(order_id=fulfillment.order_id)
+        if wms_document:
+            wms_document.delete()
+
+    def order_fulfilled(
+        self,
+        order,
+        previous_value,
+    ):
+        user_id = self.requestor.id
+        # Check if there is already a wms document and delete if true
+        wms_document = WmsDocument.objects.filter(order=order)
+        if wms_document:
+            wms_document.delete()
+        # Create GRN document
+        with transaction.atomic():
+            wms_document = wms_document_create(
+                order=order,
+                document_type='GRN',
+                created_by_id=user_id
+            )
+            wms_positions_bulk_create(order=order, wms_document_id=wms_document.id)
+
+
+def wms_document_generate_number():
+    now = datetime.now()
+    current_year = int(now.strftime("%Y"))
+
+    last_wms_document = WmsDocument.objects.filter().last()
+    match = re.search("(\d+)/(\d+)", last_wms_document.number)
+    if match:
+        number, year = int(match.group(1)), int(match.group(2))
+        if current_year == year and number:
+            new_number = number + 1
+            return f"WZ-S-{new_number}/{current_year}"
+    return f"WZ-S-1/{current_year}"
+
+
+def wms_document_create(
+    order: "Order",
+    document_type: str,
+    created_by_id: int
+):
+    warehouse = Warehouse.objects.filter().first()
+    number = wms_document_generate_number()
+
+    return WmsDocument.objects.create(
+        document_type=document_type,
+        number=number,
+        status='APPROVED',
+        created_by_id=created_by_id,
+        recipient_email=order.user_email,
+        warehouse=warehouse,
+        location='',
+        order_id=order.pk
+    )
+
+
+def wms_create_position(order_line: "OrderLine", wms_document_id: str) -> "WmsDocPosition":
+    quantity = order_line.quantity
+    product_variant = order_line.variant
+    weight = order_line.variant.product.weight.kg
+    return WmsDocPosition(
+        quantity=quantity,
+        product_variant=product_variant,
+        weight=weight,
+        document_id=wms_document_id
+    )
+
+
+def wms_positions_bulk_create(order: "Order", wms_document_id: str) -> None:
+    order_lines = OrderLine.objects.filter(order=order)
+    wms_positions = []
+    for order_line in order_lines:
+        wms_position = wms_create_position(
+            order_line=order_line,
+            wms_document_id=wms_document_id
+        )
+        wms_positions.append(wms_position)
+
+    WmsDocPosition.objects.bulk_create(wms_positions)
