8f1cb8105b7873fc7f6733996904bf100dba0a10
Michał Dąbrowski <michal0211@gmail.com>
2024-11-05 14:19:14 +0100

    vercel deployments

--- saleor/settings.py
+++ saleor/settings.py
@@ -838,7 +838,8 @@ EXTERNAL_PLUGINS = [
     "saleor_gs.saleor.plugins.cod.plugin.CodGatewayPlugin",
     "saleor_gs.saleor.plugins.payu.plugin.PayuGatewayPlugin",
     "saleor_gs.saleor.plugins.erp_next.plugin.ErpNextPlugin",
-    "saleor_gs.saleor.plugins.invoicing.plugin.InvoicingPlugin"
+    "saleor_gs.saleor.plugins.invoicing.plugin.InvoicingPlugin",
+    "saleor_gs.saleor.plugins.vercel.plugin.VercelPlugin"
 ]
 installed_plugins = pkg_resources.iter_entry_points("saleor.plugins")
 for entry_point in installed_plugins:
